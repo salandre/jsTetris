64c809f068b7d3b957fdf069512432aad006bdec
salandre <benoit.salandre@gmail.com>
2022-03-21 16:49:14 +0100

    First commit

---
+++ tetris.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    const squares = array.from(document.querySelectorAll('.grid div'));
+    const scoreDisplay = document.querySelector('#score');
+    const StartBtn = document.querySelector('#start-button');
+    const width = 10;
+
+const lTetromino = []
+
+});
